53ee7065ffb62d2c81a57bc5a38756d01229f856
Barlus Cuda <barluscuda@gmail.com>
2025-03-07 20:41:47 +0700

    add torch-tensor

---
+++ torch-tensor.py
@@ -0,0 +1,12 @@
+import torch
+
+a = torch.tensor([1, 2, 3])
+b = torch.tensor([4, 5, 6])
+
+print("a:", a)
+print("b:", b)
+
+print("a + b:", a + b)
+print("a - b:", a - b)
+print("a * b:", a * b)
+print("a / b:", a / b)
